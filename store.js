cb7417e7a7162ad7f009b90987f2abadc165f2e2
LebogangMputle <lebozikalala@gmail.com>
2024-05-22 14:25:04 +0200

    exported the update function

---
+++ store.js
@@ -0,0 +1,86 @@
+/**
+ * @typedef {object} Item
+ * @prop {number} value
+ */
+
+/**
+ * @typedef {object} State
+ * @prop {Itemtem} wind
+ * @prop {Item} temperature
+ * @prop {Item} humidity
+ */
+
+/**
+ * @callback Notify
+ * @param {State} next
+ * @param {state} prev
+ */
+
+/**
+ * @callback Action
+ * @param {State}
+ * @returns {State}
+ */
+
+/**
+ * @callback Update
+ * @param {Action}
+ */
+
+/**
+ * @callback Subscribe
+ * @param {Notify}
+ */
+
+/**
+ * @typedef {object} Store
+ * @prop {Update} update
+ * @prop {Subscribe} subscribe
+ */
+
+const initial = {
+    wind: {
+        value: 1,
+    },
+    temperature: {
+        value: 1,
+    },
+    humidity: {
+        value: 1,
+    },
+};
+
+const states = [initial];
+
+/**
+ * 
+ * @type {Array<Notify>} 
+ */
+
+const notifier = [];
+
+const Notification = [];
+
+export const update = (action) => {
+        if (typeof action !== 'function') {
+            throw new Error("action is required to be function")
+        }
+        //object.freeze means it cant be modified
+        const prev = Object.freeze({...states[0]}); //... makes sure they cant be mutated.
+        const next = Object.freeze({...action(prev)});
+
+        states.unshift(next)    //move it to the front
+    };
+
+/**
+ * @param {Notify} handler
+ * @returns  
+ */
+export const subscribe = (Notify) => {
+    notifier.push(Notify);
+
+    const unsubscribe = () => {
+
+    }
+    return unsubscribe;
+};
